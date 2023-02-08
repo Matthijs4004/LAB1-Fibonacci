760e9a1a8a0d3d515821bd4d5cec10da2d2d42ef
Matthijs4004 <matthijs4004@gmail.com>
2023-02-08 10:07:03 +0100

    shows on index.html instead of console

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+    
+    let number1 = 0 
+    let number2 = 1
+    let number3
 
-let number1 = 0 
-let number2 = 1
-let number3
-
-for ( var i = 1; i <= 20; i++ ) {
-    console.log( number1 )
-    number3 = number1 + number2
-    number1 = number2
-    number2 = number3
-}
+    for ( var i = 1; i <= 20; i++ ) {
+        document.getElementById( 'Fibonacci' ).innerText += "  "  + number1
+        number3 = number1 + number2
+        number1 = number2
+        number2 = number3
+    }
+});
